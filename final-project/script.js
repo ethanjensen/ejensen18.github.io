f0a5d48601a231e168dc31a8805f4b86a0b35604
ethanjensn2217 <ejensen18errgefox.edu>
2021-12-02 21:56:23 -0800

    Added Buildings to buy

--- final-project/script.js
+++ final-project/script.js
@@ -1,5 +1,5 @@
 getRequest(0);
-document.querySelector("#jorr-picasso").onclick = (event) => 
+document.querySelector("#jorr-picasso").onclick = (event) =>
 {
     parse(event);
 };
@@ -26,6 +26,12 @@ function displayResults(results)
 numTokens = 0;
 buildings = [0,0,0,0,0,0];
 buildingPrices = [10,100,1000,10000,100000,1000000]
+buildingNames = ["parseInt", "Regular expressions", "JSON.parse()", "xml parser", "compiler", "natural language processing"];
+for (let i = 0; i < buildings.length; i++)
+{
+  displayCost(i);
+  document.getElementById(buildingNames[i]).addEventListener("click", function(){buyBuilding(i);});
+}
 
 function parse(event)
 {
@@ -38,19 +44,54 @@ function parse(event)
 }
 
 updateNumber = setInterval(frame, 5);
+let framesElapsed = 0;
 
 function frame()
 {
   document.getElementById("number-display").innerText = "Parsed Tokens:" + numTokens.toString();
+  if (framesElapsed % 200 == 0)
+  {
+    numTokens += buildings[0];
+  }
+  if (framesElapsed % 100 == 0)
+  {
+    numTokens += buildings[1];
+  }
+  if (framesElapsed % 50 == 0)
+  {
+    numTokens += buildings[2];
+  }
+  if (framesElapsed % 25 == 0)
+  {
+    numTokens += buildings[3];
+  }
+  if (framesElapsed % 12 == 0)
+  {
+    numTokens += buildings[4];
+  }
+  if (framesElapsed % 6 == 0)
+  {
+    numTokens += buildings[5];
+  }
+  framesElapsed += 1;
 }
 
 function buyBuilding(n)
 {
+  console.log(n);
   let price =  buildingPrices[n]
   if (numTokens >= Math.ceil(buildingPrices[n]))
   {
+    numTokens -= Math.ceil(buildingPrices[n]);
     buildingPrices[n] = price*1.05
+    buildings[n] += 1;
   }
+  displayCost(n);
+}
+
+function displayCost(n)
+{
+  document.getElementById(buildingNames[n]).innerText = "Buy " + buildingNames[n] + "    Price: " + Math.ceil(buildingPrices[n]);
 }
 
 function sprayToken(event)
@@ -59,18 +100,27 @@ function sprayToken(event)
   document.body.appendChild(div);
   var clientX = event.clientX;
   var clientY = event.clientY;
-  console.log(clientX, clientY);
   //position div on mouseX, mouseY
   div.style.position = "absolute";
   div.style.left = clientX.toString() + "px";
   div.style.top = clientY.toString() + "px";
   div.innerHTML = "<img src = \"parsedToken.png\">";
   div.style.zIndex = 1;
-  console.log(div.style.top);
   //animate with sprayFrame() and then this.remove()
-}
-
-function sprayFrame()
-{
-  
+  let id = setInterval(sprayFrame, 10);
+  let n = 50
+  let angle = Math.random()*360;
+  function sprayFrame()
+  {
+    let dx = Math.cos(angle);
+    let dy = Math.sin(angle);
+    div.style.left = (parseInt(div.style.left) + dx).toString() + "px";
+    div.style.top = (parseInt(div.style.top) + dy).toString() + "px";
+    if (n < 1)
+    {
+      clearInterval(id);
+      document.body.removeChild(div);
+    }
+    n -= 1;
+  }
 }
